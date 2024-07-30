b8f27722b3939f7db887ce1eb6a236796a67bd38
alkausarhapis <alkausarhapis@gmail.com>
2024-07-30 23:09:33 +0700

    fix config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    colors: {
-      primary: "#4ba3c3",
-      secondary: "#000000",
-      tertiary: "#96A3A6",
-      paragraph: "#747c92",
-      highlight: "#ff5964",
-      white: "#ffffff",
+    container: {
+      center: true,
     },
-    extend: {},
+    extend: {
+      boxShadow: {
+        cust: "0 4px 8px 3px rgba(0, 0, 0, 0.1)",
+      },
+      colors: {
+        primary: "#4ba3c3",
+        secondary: "#000000",
+        tertiary: "#96A3A6",
+        paragraph: "#747c92",
+        highlight: "#ff5964",
+      },
+    },
   },
   plugins: [],
 };
